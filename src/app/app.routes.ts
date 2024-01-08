849346dca6ab323867bdd1d14300c6bb446b9294
werterpires <werterpires23@hotmail.com>
2024-01-08 04:49:45 -0300

    make initial changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { ContainerComponent } from './components/shared/container/container.component';
+import { RegistersComponent } from './components/registers/registers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: RegistersComponent }];
