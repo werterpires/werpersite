ccd07d231ae6c4574ee4a018b1be1e9266520c44
werterpires <werterpires23@hotmail.com>
2024-01-08 17:46:05 -0300

    configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ContainerComponent } from './components/shared/container/container.component';
 import { RegistersComponent } from './components/registers/registers.component';
+import { LogonComponent } from './components/auth/logon/logon.component';
 
 export const routes: Routes = [
+  { path: 'logon', component: LogonComponent },
   { path: 'cadastros', component: RegistersComponent },
   { path: '', component: RegistersComponent },
 ];
