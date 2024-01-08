b78bd9bce9f79a4d20ecc7d6ae8f6aecf06e4b52
werterpires <werterpires23@hotmail.com>
2024-01-08 17:46:13 -0300

    add logon component

---
+++ src/app/components/auth/logon/logon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logon',
+  standalone: true,
+  imports: [],
+  templateUrl: './logon.component.html',
+  styleUrl: './logon.component.css'
+})
+export class LogonComponent {
+
+}
