a4752d718600a2e9ed378c42b7ae98dd5bb60cf1
werterpires <werterpires23@hotmail.com>
2024-01-08 17:37:55 -0300

    fix routs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,7 @@ import { Routes } from '@angular/router';
 import { ContainerComponent } from './components/shared/container/container.component';
 import { RegistersComponent } from './components/registers/registers.component';
 
-export const routes: Routes = [{ path: '', component: RegistersComponent }];
+export const routes: Routes = [
+  { path: 'cadastros', component: RegistersComponent },
+  { path: '', component: RegistersComponent },
+];
