533629a444a2207fd4b5c19c998ac7e654077f24
werterpires <werterpires23@hotmail.com>
2024-01-08 05:00:03 -0300

    add contentManager component

---
+++ src/app/components/shared/content-manager/content-manager.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-manager',
+  standalone: true,
+  imports: [],
+  templateUrl: './content-manager.component.html',
+  styleUrl: './content-manager.component.css'
+})
+export class ContentManagerComponent {
+
+}
