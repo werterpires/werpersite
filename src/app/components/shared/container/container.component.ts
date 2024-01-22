cfeb58eeb716b90dd66f02617c3918aa013ae9dd
werterpires <werterpires23@hotmail.com>
2024-01-22 14:07:02 -0300

    add geg users info

--- src/app/components/shared/container/container.component.ts
+++ src/app/components/shared/container/container.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SideBarComponent } from '../side-bar/side-bar.component';
+import { AuthService } from '../sharedServices/auth.service';
+import { IUserFromJwt } from '../sharedTypes';
 
 @Component({
   selector: 'app-container',
@@ -8,4 +10,15 @@ import { SideBarComponent } from '../side-bar/side-bar.component';
   templateUrl: './container.component.html',
   styleUrl: './container.component.css',
 })
-export class ContainerComponent {}
+export class ContainerComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+
+  user: IUserFromJwt | null = null;
+
+  ngOnInit(): void {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      this.user = JSON.parse(atob(accessToken.split('.')[1])) as IUserFromJwt;
+    }
+  }
+}
