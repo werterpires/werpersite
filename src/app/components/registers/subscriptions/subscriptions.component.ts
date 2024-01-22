0d116c1d0e3756fabcde154c7c985e090a2ed6a8
werterpires <werterpires23@hotmail.com>
2024-01-22 15:10:11 -0300

    add subscriptions component

---
+++ src/app/components/registers/subscriptions/subscriptions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscriptions',
+  standalone: true,
+  imports: [],
+  templateUrl: './subscriptions.component.html',
+  styleUrl: './subscriptions.component.css'
+})
+export class SubscriptionsComponent {
+
+}
