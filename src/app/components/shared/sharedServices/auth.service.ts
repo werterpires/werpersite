30e5821e44eac6946a87febca99da84431537c62
werterpires <werterpires23@hotmail.com>
2024-01-24 15:39:39 -0300

    add get token

--- src/app/components/shared/sharedServices/auth.service.ts
+++ src/app/components/shared/sharedServices/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { IUserFromJwt } from '../sharedTypes';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -19,5 +20,11 @@ export class AuthService {
 
   setUser(user: IUserFromJwt | null) {
     this.user.next(user);
+  }
+
+  getHeadObject(): HttpHeaders {
+    const token = localStorage.getItem('accessToken');
+    const headObj = new HttpHeaders().set('Authorization', 'bearer ' + token);
+    return headObj;
   }
 }
