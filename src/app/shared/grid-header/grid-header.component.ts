f233d921ad9a3ca73a5d158be0f984c1425869f3
werterpires <werterpires23@hotmail.com>
2024-01-24 14:05:55 -0300

    add grid header

---
+++ src/app/shared/grid-header/grid-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './grid-header.component.html',
+  styleUrl: './grid-header.component.css'
+})
+export class GridHeaderComponent {
+
+}
