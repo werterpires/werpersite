4bfd10ea765cca541451f2fb4c559d72393cb5b5
werterpires <werterpires23@hotmail.com>
2024-01-25 10:38:31 -0300

    add create form

---
+++ src/app/components/shared/create-form/create-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-form.component.html',
+  styleUrl: './create-form.component.css'
+})
+export class CreateFormComponent {
+
+}
