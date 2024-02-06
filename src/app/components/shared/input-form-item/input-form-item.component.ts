5a0345ca507a163c578e90bf86ee4a6e61d914ef
werterpires <werterpires23@hotmail.com>
2024-02-05 21:08:45 -0300

    add new inputs in input form item component

--- src/app/components/shared/input-form-item/input-form-item.component.ts
+++ src/app/components/shared/input-form-item/input-form-item.component.ts
@@ -3,17 +3,27 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IFormError } from '../form-error/types';
 import { FormErrorComponent } from '../form-error/form-error.component';
+import { TimeStampsPipe } from '../../../pipes/time-stamps.pipe';
+import { MoneyPipe } from '../../../pipes/money.pipe';
 
 @Component({
   selector: 'app-input-form-item',
   standalone: true,
-  imports: [NgClass, FormsModule, NgIf, FormErrorComponent],
+  imports: [
+    NgClass,
+    FormsModule,
+    NgIf,
+    FormErrorComponent,
+    TimeStampsPipe,
+    MoneyPipe,
+  ],
   templateUrl: './input-form-item.component.html',
   styleUrl: './input-form-item.component.css',
 })
 export class InputFormItemComponent {
   @Input() idx: number = 0;
   @Input() value: string = '';
+  @Input() numberValue: number = 0;
   @Input() label: string | null = null;
   @Input() inputClass: string[] = ['mediumField'];
   @Input() inputType: string = 'text';
@@ -22,6 +32,8 @@ export class InputFormItemComponent {
     errorText: ['ocorreu um erro'],
     active: [],
   };
+  @Input() disabled = false;
+  @Input() pipe: string = '';
 
   @Output() blurEmitter = new EventEmitter<void>();
   @Output() inputEmitter = new EventEmitter<string>();
