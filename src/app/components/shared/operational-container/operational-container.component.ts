a843c982b40e8fa7a95cfd36ee29b9de9f993f64
werterpires <werterpires23@hotmail.com>
2024-02-06 05:01:39 -0300

    change width of several elements

--- src/app/components/shared/operational-container/operational-container.component.ts
+++ src/app/components/shared/operational-container/operational-container.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { OperationalContainerService } from './operational-container.service';
 
 @Component({
   selector: 'app-operational-container',
   standalone: true,
   imports: [],
   templateUrl: './operational-container.component.html',
-  styleUrl: './operational-container.component.css'
+  styleUrl: './operational-container.component.css',
 })
-export class OperationalContainerComponent {
+export class OperationalContainerComponent implements AfterViewInit {
+  @ViewChild('operationalContainer') operationalContainer!: ElementRef;
 
+  ngAfterViewInit(): void {
+    this.insertMaxWidth();
+  }
+  insertMaxWidth() {
+    const element = this.operationalContainer.nativeElement;
+    if (!element) {
+      return;
+    }
+    const windowWidth = window.innerWidth;
+    const rect = element.getBoundingClientRect();
+
+    const adjustedWidth = windowWidth - 40 - rect.left + 'px';
+    element.style.maxWidth = adjustedWidth;
+  }
 }
