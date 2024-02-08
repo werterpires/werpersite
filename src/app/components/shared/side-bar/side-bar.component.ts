99153497e3457efe950804bac6e10c6c66253b87
werterpires <werterpires23@hotmail.com>
2024-02-08 15:44:49 -0300

    add resize sideBar

--- src/app/components/shared/side-bar/side-bar.component.ts
+++ src/app/components/shared/side-bar/side-bar.component.ts
@@ -1,4 +1,13 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { MenuItem } from './types';
 import { NgFor, NgIf } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
@@ -11,8 +20,31 @@ import { IUserFromJwt } from '../sharedTypes';
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css',
 })
-export class SideBarComponent {
+export class SideBarComponent implements AfterViewInit {
+  @ViewChild('sideBar') sideBar!: ElementRef;
+
   @Input() menuItens: MenuItem[] = [];
   @Output() choiceEmitter = new EventEmitter<string>();
   @Input() user: IUserFromJwt | null = null;
+
+  @HostListener('window:resize', ['$event'])
+  ngAfterViewInit(): void {
+    this.insertMaxWidth();
+  }
+  onWindowResize(event: Event): void {
+    this.insertMaxWidth();
+  }
+  insertMaxWidth() {
+    const element = this.sideBar.nativeElement;
+    if (!element) {
+      return;
+    }
+    const windowHeight = window.innerHeight;
+
+    const rect = element.getBoundingClientRect();
+
+    const adjustedHeight = windowHeight - 21 - rect.top + 'px';
+
+    element.style.height = adjustedHeight;
+  }
 }
